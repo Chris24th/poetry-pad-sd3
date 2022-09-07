5725a1cb859303426dfed25ade9a516a2dab0d82
Christopher E <christopherespenida@gmail.com>
2022-09-07 02:29:59 -0800

    commit

--- app/Http/Controllers/PoemController.php
+++ app/Http/Controllers/PoemController.php
@@ -11,7 +11,21 @@ class PoemController extends Controller
         $poem->privacy = $req->privacy;
         $poem->isDraft = $req->isDraft;
         $poem->Stanza1Line1 = $req->Stanza1Line1;
+        $poem->Stanza1Line1 = $req->Stanza1Line2;
+        $poem->Stanza1Line1 = $req->Stanza1Line3;
+        $poem->Stanza1Line1 = $req->Stanza1Line4;
+        $poem->Stanza1Line2 = $req->Stanza1Line1;
         $poem->Stanza1Line2 = $req->Stanza1Line2;
+        $poem->Stanza1Line2 = $req->Stanza1Line3;
+        $poem->Stanza1Line2 = $req->Stanza1Line4;
+        $poem->Stanza1Line3 = $req->Stanza1Line1;
+        $poem->Stanza1Line3 = $req->Stanza1Line2;
+        $poem->Stanza1Line3 = $req->Stanza1Line3;
+        $poem->Stanza1Line3 = $req->Stanza1Line4;
+        $poem->Stanza1Line4 = $req->Stanza1Line1;
+        $poem->Stanza1Line4 = $req->Stanza1Line2;
+        $poem->Stanza1Line4 = $req->Stanza1Line3;
+        $poem->Stanza1Line4 = $req->Stanza1Line4;
         $poem->save();
         
         return $poem;
