10998d11c808bf527f6383bd0cbf625a1022b0b0
Christopher E <christopherespenida@gmail.com>
2022-09-07 17:10:31 -0800

    about us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import { FaFacebook } from "react-icons/fa";
 import { AiFillTwitterCircle, AiFillInstagram } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { GiFeather } from "react-icons/gi";
 
 const About = () => {
   return (
@@ -9,23 +10,22 @@ const About = () => {
       <Row style={{ justifyContent: "space-between", marginBottom: "50px" }}>
         <Col xs={5}>
           <h1 className="about-hdr mb-4">About Us</h1>
-          <p className="about-p">
+          <p className="about-p mb-5">
             We are a startup web app with a goal to provide poem enthusiasts a
             place where they can read poetry pieces and at the same time create
             one for themselves.
           </p>
-        </Col>
-        <Col xs={5}>
-          <h1 className="about-hdr">Services</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={5}>
           <h1 className="about-hdr mb-4">Contact Us</h1>
-          <div className="about-p">
-            <p>+63 927 987 6345</p>
-            <p>poetrypad@gmail.com</p>
+          <div
+            className="about-p"
+            style={{ fontWeight: "100", fontSize: "17px" }}
+          >
             <p>
+              +63 927 987 6345
+              <br />
+              poetrypad@gmail.com
+            </p>
+            <p style={{ fontSize: "15px" }}>
               Cebu Institute of Technology-University, Cebu City, Cebu,
               Philippines
             </p>
@@ -49,7 +49,35 @@ const About = () => {
             </a>
           </div>
         </Col>
+        <Col xs={5}>
+          <h1 className="about-hdr mb-4">Services</h1>
+          <p className="about-p">
+            <GiFeather size={30} style={{ marginRight: "10px" }} />
+            read poems of fellow poets
+          </p>
+          <p className="about-p">
+            <GiFeather size={30} style={{ marginRight: "10px" }} />
+            write your own poems
+          </p>
+          <p className="about-p">
+            <GiFeather size={30} style={{ marginRight: "10px" }} />
+            like and comment on poems
+          </p>
+          <p className="about-p">
+            <GiFeather size={30} style={{ marginRight: "10px" }} />
+            search for synonyms & antonyms
+          </p>
+          <p className="about-p">
+            <GiFeather size={30} style={{ marginRight: "10px" }} />
+            find rhyming words
+          </p>
+          <p className="about-p">
+            <GiFeather size={30} style={{ marginRight: "10px" }} />
+            automatically check plagiarism
+          </p>
+        </Col>
       </Row>
+      <Col xs={5}></Col>
     </Container>
   );
 };
--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -11,6 +11,8 @@ import {
   Col,
 } from "react-bootstrap";
 import Logo from "../images/logo.svg";
+import { AiFillHome } from "react-icons/ai";
+// import { HiBars3BottomRight } from "react-icons/hi";
 
 const Sidebar = () => {
   const [show, setShow] = useState(false);
@@ -19,34 +21,38 @@ const Sidebar = () => {
     <>
       {show ? (
         <div fluid className="sidebar-container-s">
-          <Nav
-            defaultActiveKey="/dashboard"
-            className="flex-column sidebar-nav"
-          >
-            <Row className="sidebar-row-btn">
-              <Button onClick={() => setShow(!show)} className="sidebar-btn">
+          <Nav defaultActiveKey="/dashboard" className="flex-column nav-s">
+            <div className="sidebar-row-btn">
+              <button onClick={() => setShow(!show)} className="sidebar-btn">
                 show/hide
-              </Button>
-            </Row>
-            <div className="sidebar-nav ">
-              <Nav.Link href="/dashboard" className="sidebar-navlink">
+              </button>
+              {/* <HiBars3BottomRight /> */}
+            </div>
+            <Row className="sidebar-navlink-cont ">
+              <Nav.Link
+                href="/dashboard"
+                className="sidebar-navlink"
+                style={{ color: "black", fontWeight: "bold" }}
+              >
                 icon Active
               </Nav.Link>
               <Nav.Link href="/dashboard" className="sidebar-navlink">
                 icon Link
               </Nav.Link>
-            </div>
+            </Row>
           </Nav>
         </div>
       ) : (
         <div fluid className="sidebar-container-h">
-          <Nav
-            defaultActiveKey="/dashboard"
-            className="flex-column sidebar-nav"
-          >
-            <Button onClick={() => setShow(!show)}>show/hide</Button>
-            <div className="sidebar-nav ">
+          <Nav defaultActiveKey="/dashboard" className="flex-column nav-h">
+            <div className="sidebar-row-btn">
+              <button onClick={() => setShow(!show)} className="sidebar-btn">
+                //
+              </button>
+            </div>
+            <div className="sidebar-navlink-cont ">
               <Nav.Link href="/dashboard" className="sidebar-navlink">
+                <AiFillHome />
                 icon
               </Nav.Link>
               <Nav.Link href="/dashboard" className="sidebar-navlink">
