3311d0767b3141bf51e0cb1c16b36fa8cef2e665
Christopher E <christopherespenida@gmail.com>
2022-09-07 18:04:29 -0800

    res

--- src/pages/Sidebar.jsx
+++ src/pages/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container, Form, Button, Row, Col } from "react-bootstrap";
 import Logo from "../images/logo.svg";
 import { Link } from "react-router-dom";
@@ -7,48 +7,60 @@ import { AiFillHome } from "react-icons/ai";
 
 const Sidebar = () => {
   const [show, setShow] = useState(false);
+  const [size, setSize] = useState();
+
+  useEffect(() => {
+    setSize(window.innerWidth);
+    // return () => {
+    //   setSize(window.innerWidth);
+    // };
+  });
 
   return (
     <>
-      {show ? (
-        <div className="sidebar-container-s">
-          <Container className="sb-cont-s">
-            <div className="sb-btn-cont">
-              <span onClick={() => setShow(!show)} className="sb-btn">
-                hide
-              </span>
-              {/* <HiBars3BottomRight /> */}
+      {size > 600 ? (
+        <>
+          {show ? (
+            <div className="sidebar-container-s">
+              <Container className="sb-cont-s">
+                <div className="sb-btn-cont">
+                  <span onClick={() => setShow(!show)} className="sb-btn">
+                    hide
+                  </span>
+                  {/* <HiBars3BottomRight /> */}
+                </div>
+                <div className="sb-link-cont">
+                  <Link to="/dashboard" className="sb-link">
+                    icon Active
+                  </Link>
+                  <Link to="/dashboard" className="sb-link">
+                    icon Link
+                  </Link>
+                </div>
+              </Container>
             </div>
-            <div className="sb-link-cont">
-              <Link to="/dashboard" className="sb-link">
-                icon Active
-              </Link>
-              <Link to="/dashboard" className="sb-link">
-                icon Link
-              </Link>
+          ) : (
+            <div className="sidebar-container-h">
+              <Container className="sb-cont-h">
+                <div className="sb-btn-cont">
+                  <span onClick={() => setShow(!show)} className="sb-btn">
+                    show
+                  </span>
+                  {/* <HiBars3BottomRight /> */}
+                </div>
+                <div className="sb-link-cont">
+                  <Link to="/dashboard" className="sb-link">
+                    ic
+                  </Link>
+                  <Link to="/dashboard" className="sb-link">
+                    ic
+                  </Link>
+                </div>
+              </Container>
             </div>
-          </Container>
-        </div>
-      ) : (
-        <div className="sidebar-container-h">
-          <Container className="sb-cont-h">
-            <div className="sb-btn-cont">
-              <span onClick={() => setShow(!show)} className="sb-btn">
-                show
-              </span>
-              {/* <HiBars3BottomRight /> */}
-            </div>
-            <div className="sb-link-cont">
-              <Link to="/dashboard" className="sb-link">
-                ic
-              </Link>
-              <Link to="/dashboard" className="sb-link">
-                ic
-              </Link>
-            </div>
-          </Container>
-        </div>
-      )}
+          )}
+        </>
+      ) : null}
     </>
   );
 };
