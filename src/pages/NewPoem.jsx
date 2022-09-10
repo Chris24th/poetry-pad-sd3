ca6ad2b79e7d2629c1e71c3f2d3bded2264cfd1b
Christopher E <christopherespenida@gmail.com>
2022-09-10 15:12:22 -0800

    .

--- src/pages/NewPoem.jsx
+++ src/pages/NewPoem.jsx
@@ -3,23 +3,36 @@ import React from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 
 const NewPoem = () => {
+  const onAdd = () => {};
   return (
     <div>
       <Row>
-        <Col md={4}>
+        <Col md={3}>
           <Sidebar />
         </Col>
-        <Col md={4}>
+        <Col md={6}>
           <Row className="justify-content-center">
             <Form>
-              <Form.Group>
-                <Form.Label>Title</Form.Label>
-                <Form.Control></Form.Control>
+              <Form.Group className="mb-3">
+                <Form.Label>Poem Title</Form.Label>
+                <Form.Control
+                  type="text"
+                  size="lg"
+                  placeholder="Poem Title"
+                ></Form.Control>
               </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Control
+                  as="textarea"
+                  rows={4}
+                  placeholder="1st Stanza"
+                ></Form.Control>
+              </Form.Group>
+              <button onClick={onAdd}> Add Stanza + </button>
             </Form>
           </Row>
         </Col>
-        <Col md={4}>
+        <Col md={3}>
           <Row className="flex-column ">
             <Col style={{ textAlign: "end" }}>
               x<br />x<br />x
