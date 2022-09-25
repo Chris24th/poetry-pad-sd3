18afd57ce3502e04b117b9b0453bdba66f4eaa2c
Christopher E <christopherespenida@gmail.com>
2022-09-25 13:41:37 -0800

    .

--- app/Http/Controllers/LikePoemController.php
+++ app/Http/Controllers/LikePoemController.php
@@ -9,23 +9,33 @@ use Illuminate\Support\Facades\DB;
 
 class LikePoemController extends Controller
 {
-    function createlikePoem(Request $req){
+    function createlikePoem(Request $req)
+    {
         $checker = likePoem::where('idUser', $req->idUser)->first();
-        if($checker){
+        if ($checker) {
             likePoem::where('idUser', $req->idUser)->delete();
-            return ['message'=>'Unliked']; 
-        }
-        else{
+            return ['message' => 'Unliked'];
+        } else {
             $likePoem = new likePoem;
             $likePoem->idPoem = $req->idPoem;
             $likePoem->idUser = $req->idUser;
             $likePoem->save();
-            return $likePoem;  
+            return $likePoem;
         }
     }
-    function displaylikePoem(){
-        $likePoem = DB::table('like_poems')->get();
-        $user = User::where('id', $likePoem->idUser)->first();
-        return $user->penName;
+    function displaylikePoem()
+    {
+        $minID = Poem::min('id');
+        $userArr = array();
+
+        for ($i = $minID; $i > 0; $i--) {
+            $data = User::where('id', $i)->first();
+            if ($data) {
+                array_push($userArr, $data);
+            }
+        }
+        if ($userArr)
+            return $userArr;
+        else return ['error' => 'empty data'];
     }
 }
