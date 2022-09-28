fa31c3732fd8c7d76fcfaf695aefe77bea73e946
Christopher E <christopherespenida@gmail.com>
2022-09-28 12:56:11 -0800

    .

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,30 +4,37 @@ namespace App\Http\Controllers;
 
 use App\Models\Comment;
 use App\Models\likeComment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class CommentController extends Controller
 {
-    function createcomment(Request $req){
+    function createcomment(Request $req)
+    {
         $comment = new Comment();
         $comment->idPoem = $req->idPoem;
         $comment->idUser = $req->idUser;
+        $user = User::where('id', $req->idUser);
         $comment->textContent = $req->textContent;
         $comment->save();
-        return $comment;
+        $data = [$comment, $user];
+        return $data;
     }
-    function editcomment(Request $req){
+    function editcomment(Request $req)
+    {
         $comment = Comment::where('id', $req->idComment)->first();
         $comment->textContent = $req->textContent;
         $comment->save();
         return $comment;
     }
-    function deletecomment(Request $req){
+    function deletecomment(Request $req)
+    {
         $comment = Comment::where('id', $req->idComment)->delete();
-        return ['message'=>'Comment deleted successfully.'];
+        return ['message' => 'Comment deleted successfully.'];
     }
-    function displaycomment(){
+    function displaycomment()
+    {
         $comment = DB::table('comments')->get();
         return $comment;
     }
